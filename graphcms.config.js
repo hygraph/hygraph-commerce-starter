7a9114564431b81f339b45a0a3de0d28377bab56
Jonathan Steele <jonathan@steele.pro>
2021-02-02 13:10:14 +0000

    chore: Configure localisation

---
+++ graphcms.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  locales: [
+    {
+      currency: 'GBP',
+      value: 'en',
+      label: 'English',
+      default: true
+    },
+    {
+      currency: 'EUR',
+      value: 'de',
+      label: 'German'
+    }
+  ]
+}

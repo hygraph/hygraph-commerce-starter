8eed5b9d501eb0ccd59b36561a3d65d824d11042
Jonathan Steele <jonathan@steele.pro>
2021-02-05 16:22:25 +0000

    refactor: Use existing <ChevronDownIcon /> component

--- components/product-page-ui.js
+++ components/product-page-ui.js
@@ -2,6 +2,8 @@ import * as React from 'react'
 import { useRouter } from 'next/router'
 import Image from 'next/image'
 
+import { ChevronDownIcon } from '@/icons'
+
 function ProductPageUI({ product }) {
   const router = useRouter()
   const [variantQuantity, setVariantQuantity] = React.useState(1)
@@ -72,14 +74,11 @@ function ProductPageUI({ product }) {
                     </option>
                   ))}
                 </select>
-                <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-4 text-slategray">
-                  <svg
-                    className="fill-current h-4 w-4"
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 20 20"
-                  >
-                    <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />
-                  </svg>
+                <div className="pointer-events-none absolute inset-y-0 right-0 px-2 flex items-center">
+                  <ChevronDownIcon
+                    className="h-4 w-4 text-gray-400"
+                    aria-hidden="true"
+                  />
                 </div>
               </div>
             </div>
@@ -109,14 +108,11 @@ function ProductPageUI({ product }) {
                   )
                 })}
               </select>
-              <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-4 text-slategray">
-                <svg
-                  className="fill-current h-4 w-4"
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 20 20"
-                >
-                  <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />
-                </svg>
+              <div className="pointer-events-none absolute inset-y-0 right-0 px-2 flex items-center">
+                <ChevronDownIcon
+                  className="h-4 w-4 text-gray-400"
+                  aria-hidden="true"
+                />
               </div>
             </div>
           </div>

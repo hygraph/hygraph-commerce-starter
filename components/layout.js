550baa7ccd146dfb813655dea5c6d1a6c8b9aab2
Jonathan Steele <jonathan@steele.pro>
2021-02-23 22:28:44 +0000

    feat: Add currency switch

--- components/layout.js
+++ components/layout.js
@@ -5,8 +5,10 @@ import Link from 'next/link'
 import { ChevronDownIcon } from '@/icons'
 import { GraphCMSSVG } from '@/svgs'
 import { locales } from 'graphcms.config'
+import { useCurrencyContext } from './context/currency'
 
 function Layout({ children, navigation }) {
+  const { activeCurrency, switchCurrency } = useCurrencyContext()
   const router = useRouter()
 
   const activeLocale = locales.find((locale) => locale.value === router.locale)
@@ -15,6 +17,14 @@ function Layout({ children, navigation }) {
     const path = ['/cart'].includes(router.asPath) ? router.asPath : '/'
 
     router.push(path, path, { locale: event.target.value })
+  }
+
+  const updateCurrency = (event) => {
+    const currency = navigation.currencies.find(
+      (currency) => currency.code === event.target.value
+    )
+
+    switchCurrency(currency)
   }
 
   return (
@@ -44,7 +54,7 @@ function Layout({ children, navigation }) {
               </ul>
             ) : null}
             <div className="flex items-center">
-              <form className="sm:max-w-xs">
+              <form className="flex sm:max-w-xs">
                 <fieldset className="w-full">
                   <label htmlFor="language" className="sr-only">
                     Language
@@ -71,6 +81,34 @@ function Layout({ children, navigation }) {
                     </div>
                   </div>
                 </fieldset>
+                {navigation?.currencies.length ? (
+                  <fieldset className="w-full">
+                    <label htmlFor="language" className="sr-only">
+                      Language
+                    </label>
+                    <div className="relative">
+                      <select
+                        id="language"
+                        name="language"
+                        value={activeCurrency.code}
+                        className="block appearance-none bg-white border-none px-4 py-0 pr-8 focus:outline-none focus:bg-white text-lightgray focus:text-slategray rounded-lg"
+                        onChange={updateCurrency}
+                      >
+                        {navigation.currencies.map((currency) => (
+                          <option key={currency.id} value={currency.code}>
+                            {currency.code}
+                          </option>
+                        ))}
+                      </select>
+                      <div className="pointer-events-none absolute inset-y-0 right-0 px-2 flex items-center">
+                        <ChevronDownIcon
+                          className="h-4 w-4 text-gray-400"
+                          aria-hidden="true"
+                        />
+                      </div>
+                    </div>
+                  </fieldset>
+                ) : null}
               </form>
               <Link href="/cart">
                 <a>Cart</a>

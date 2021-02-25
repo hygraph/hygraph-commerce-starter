0a6bb31b4c98731108ef76845c3bdd051bedf041
Jonathan Steele <jonathan.steele@graphcms.com>
2021-02-25 15:40:36 +0000

    feat: Nicer cart UI (#13)
    * feat: Nice cart UI
    
    * feat: Add cart total to header

--- components/icons/index.js
+++ components/icons/index.js
@@ -1,1 +1,4 @@
 export { default as ChevronDownIcon } from './chevron-down'
+export { default as ChevronUpSmallIcon } from './chevron-up-small'
+export { default as ShoppingCartIcon } from './shopping-cart'
+export { default as XSmallIcon } from './x-small'
--- pages/cart.js
+++ pages/cart.js
@@ -1,9 +1,15 @@
+import * as React from 'react'
 import Image from 'next/image'
 import Link from 'next/link'
 import { useRouter } from 'next/router'
 import { useCart } from 'react-use-cart'
 import { loadStripe } from '@stripe/stripe-js'
 
+import {
+  ChevronDownIcon,
+  ChevronUpSmallIcon,
+  XSmallIcon
+} from '@/components/icons'
 import { formatCurrencyValue } from '@/utils/format-currency-value'
 import getNavigation from '@/lib/get-navigation'
 import { useSettingsContext } from '@/context/settings'
@@ -14,7 +20,7 @@ const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY)
 function Cart() {
   const {
     cartTotal,
-    emptyCart,
+    isEmpty,
     items,
     removeItem,
     updateItemQuantity
@@ -78,73 +84,98 @@ function Cart() {
     }
   }
 
+  if (isEmpty) return <p>Your cart is empty</p>
+
   return (
-    <div>
-      {submissionError && submissionState.message}
-      <button onClick={emptyCart} disabled={submissionLoading}>
-        Empty
-      </button>
+    <React.Fragment>
       {items.map((item) => {
         return (
-          <div className="flex items-center" key={item.id}>
-            <div className="w-1/6">
-              <Image
-                src={item.image.url}
-                width={item.image.width}
-                height={item.image.height}
-              />
-            </div>
-            <div>
-              <Link href={`/products/${item[router.locale].slug}`}>
-                <a>{item[router.locale].name}</a>
-              </Link>
-            </div>
-            <div className="flex items-center space-x-2">
-              <div>
-                <button
-                  onClick={() => decrementItemQuantity(item)}
-                  disabled={submissionLoading}
-                >
-                  &#8210;
-                </button>
+          <div
+            className="md:bg-gray-50 md:rounded-lg flex items-center py-3 md:py-6 md:px-6 md:mb-3"
+            key={item.id}
+          >
+            <div className="w-3/5 flex flex-grow items-center">
+              <div className="h-16 md:h-20 w-16 md:w-20 mr-4 bg-gray-50 p-1 rounded-lg">
+                <Image
+                  src={item.image.url}
+                  width={item.image.width}
+                  height={item.image.height}
+                />
               </div>
-              <span>{item.quantity}</span>
               <div>
+                <Link href={`/products/${item[router.locale].slug}`}>
+                  <a className="text-gray-800 font-medium text-sm md:text-base">
+                    {item[router.locale].name}
+                  </a>
+                </Link>
                 <button
-                  onClick={() => incrementItemQuantity(item)}
+                  className="text-gray-400 hover:text-indigo-600 text-xs flex items-center focus:outline-none"
+                  onClick={() => removeItem(item.id)}
                   disabled={submissionLoading}
                 >
-                  &#43;
+                  <XSmallIcon className="h-3 w-3" />
+                  Remove
                 </button>
               </div>
             </div>
-            <div>
+            <div className="hidden md:flex flex-col items-center ml-auto">
               <button
-                onClick={() => removeItem(item.id)}
+                className="text-gray-400 hover:text-indigo-600 focus:outline-none p-1"
+                onClick={() => incrementItemQuantity(item)}
                 disabled={submissionLoading}
               >
-                Remove
+                <ChevronUpSmallIcon className="h-4 w-4" />
+              </button>
+              <span className="mx-3 md:mx-6 p-1">{item.quantity}</span>
+              <button
+                className="text-gray-400 hover:text-indigo-600 focus:outline-none p-1"
+                onClick={() => decrementItemQuantity(item)}
+                disabled={submissionLoading}
+              >
+                <ChevronDownIcon className="h-4 w-4" />
               </button>
             </div>
-            <div>
-              {formatCurrencyValue({
-                currency: activeCurrency,
-                value: item.itemTotal
-              })}
+            <div className="text-right md:w-1/5">
+              <p className="font-medium text-gray-800">
+                {formatCurrencyValue({
+                  currency: activeCurrency,
+                  value: item.itemTotal
+                })}
+              </p>
+              {item.quantity > 1 && (
+                <p className="text-gray-400 text-sm">
+                  {formatCurrencyValue({
+                    currency: activeCurrency,
+                    value: item.price
+                  })}{' '}
+                  each
+                </p>
+              )}
             </div>
           </div>
         )
       })}
-      <p className="text-xl">
-        {formatCurrencyValue({
-          currency: activeCurrency,
-          value: cartTotal
-        })}
-      </p>
-      <button onClick={handleClick} disabled={submissionLoading}>
-        Checkout
-      </button>
-    </div>
+      <div className="mt-3 md:mt-6 py-3 md:py-6 border-t-2 border-gray-50">
+        <div className="flex flex-col items-end">
+          <div className="flex flex-col items-end mb-3">
+            <span className="text-gray-700">Sub total</span>
+            <span className="text-xl font-bold text-indigo-600">
+              {formatCurrencyValue({
+                currency: activeCurrency,
+                value: cartTotal
+              })}
+            </span>
+          </div>
+          <button
+            className="bg-indigo-600 hover:bg-gray-700 px-4 py-3 rounded-lg text-white text-sm font-bold tracking-widest uppercase focus:outline-none"
+            onClick={handleClick}
+            disabled={submissionLoading}
+          >
+            Checkout
+          </button>
+        </div>
+      </div>
+    </React.Fragment>
   )
 }
 

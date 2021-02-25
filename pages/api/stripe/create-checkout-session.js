a9dfd9b18d903eaf655e4fc0bff9a0d3e1e32acb
Jonathan Steele <jonathan@steele.pro>
2021-02-25 11:24:32 +0000

    feat: try/catch in API route

--- pages/api/stripe/create-checkout-session.js
+++ pages/api/stripe/create-checkout-session.js
@@ -1,63 +1,69 @@
-import graphcmsClient, { gql } from '@/lib/graphcms-client'
-import { ProductFragment } from '@/lib/graphql-fragments'
 import Stripe from 'stripe'
+
+import graphcmsClient, { gql } from '@/lib/graphcms-client'
 
 export default async (req, res) => {
   const stripe = new Stripe(process.env.STRIPE_SECRET_KEY)
+  try {
+    const { currency, items, locale, ...rest } = req.body
 
-  const { currency, items, locale, ...rest } = req.body
-
-  const getProduct = async (id) => {
-    const {
-      product: { description, images, name, ...product }
-    } = await graphcmsClient.request(
-      gql`
-        query ProductQuery($id: ID!, $locale: Locale!) {
-          product(where: { id: $id }, locales: [$locale]) {
-            productId: id
-            description
-            images(first: 1) {
-              url
+    const getProduct = async (id) => {
+      const {
+        product: { description, images, name, ...product }
+      } = await graphcmsClient.request(
+        gql`
+          query ProductQuery($id: ID!, $locale: Locale!) {
+            product(where: { id: $id }, locales: [$locale]) {
+              productId: id
+              description
+              images(first: 1) {
+                url
+              }
+              name
+              price
             }
-            name
-            price
           }
+        `,
+        {
+          id,
+          locale
         }
-      `,
-      {
-        id,
-        locale
+      )
+
+      return {
+        currency,
+        product_data: {
+          description,
+          metadata: {
+            ...product
+          },
+          name,
+          images: images.map((img) => img.url)
+        },
+        unit_amount: product.price
       }
+    }
+
+    const line_items = await Promise.all(
+      items.map(async (item) => ({
+        quantity: item.quantity,
+        price_data: await getProduct(item.productId)
+      }))
     )
 
-    return {
-      currency,
-      product_data: {
-        description,
-        metadata: {
-          ...product
-        },
-        name,
-        images: images.map((img) => img.url)
-      },
-      unit_amount: product.price
-    }
+    const session = await stripe.checkout.sessions.create({
+      mode: 'payment',
+      line_items,
+      locale,
+      payment_method_types: ['card'],
+      ...rest
+    })
+
+    res.status(201).json({ session })
+  } catch (error) {
+    console.error(error)
+    res.status(500).json({
+      message: 'There was a problem creating the Stripe Checkout session'
+    })
   }
-
-  const line_items = await Promise.all(
-    items.map(async (item) => ({
-      quantity: item.quantity,
-      price_data: await getProduct(item.productId)
-    }))
-  )
-
-  const session = await stripe.checkout.sessions.create({
-    mode: 'payment',
-    line_items,
-    locale,
-    payment_method_types: ['card'],
-    ...rest
-  })
-
-  res.status(201).json({ session })
 }
